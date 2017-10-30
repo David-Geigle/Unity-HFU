6a4de8032be551182a97179606e907beddab4bc3
markus0000 <33051054+markus0000@users.noreply.github.com>
2017-10-30 20:24:45 +0100

    Revert "Revert "Adding Test Scene""
    This reverts commit 9ec411c0b057ab4ca24a48f9f987819e664e5970.

---
+++ Unity-Spiel/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	
+    // Variablen
+	public GameObject player; 
+    public float yOffset, zOffset;
+   
+    
+
+
+	void Start () {
+
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+
+	void LateUpdate () {
+
+		Camera.main.transform.position = player.transform.position + new Vector3(0f,yOffset,-zOffset);
+
+	}
+
+
+}
