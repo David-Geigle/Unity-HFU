a0a5ffd9f0f4df8102024765166b64848bba5df1
David Geigle <33023777+David-Geigle@users.noreply.github.com>
2017-11-07 08:13:28 +0100

    Camera
    Camera verbessert

---
+++ Unity-Spiel/Assets/Scripts/CammeraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CammeraFollow : MonoBehaviour {
+
+	public Transform target;
+
+	public Vector3 offset;
+
+	private float currentZoom = 10f;
+	private float pitch = 2f;
+
+	void LateUpdate ()
+	{
+		transform.position = target.position - offset * currentZoom;
+		transform.LookAt(target.position + Vector3.up * pitch);
+	}
+}
---
+++ Unity-Spiel/Assets/Scripts/PlayerMovement1.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement1 : MonoBehaviour {
+
+	NavMeshAgent agent;
+
+
+	void Start ()
+	{
+		
+	}
+	
+	
+	void Update ()
+	{
+		
+	}
+}
