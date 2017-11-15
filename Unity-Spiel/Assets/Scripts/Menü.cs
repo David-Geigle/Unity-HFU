db890c60037cecae29319b47ef78a82fb4403472
MoritzM1996 <33116431+MoritzM1996@users.noreply.github.com>
2017-11-15 10:16:31 +0100

    Menü

---
+++ Unity-Spiel/Assets/Scripts/Menü.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menü : MonoBehaviour {
+    bool isMute;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+    }
+        public void NewGame()
+        {
+            Application.LoadLevel(1);
+        }
+        public void Quit()
+        {
+            Application.Quit();
+        }
+        public void Save()
+        {
+            PlayerPrefs.SetInt("currentscenesave", Application.loadedLevel);
+
+        }
+        public void Load()
+        {
+            Application.LoadLevel(PlayerPrefs.GetInt("currentscenesave"));
+        }
+    public void Mute()
+    {
+        isMute = !isMute;
+        AudioListener.volume = isMute ? 0 : 1;
+    }
+
+
+}
---
+++ Unity-Spiel/Assets/Scripts/Pause.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour {
+
+    GameObject PauseMenu;
+    bool paused;
+    bool isMute;
+
+
+	void Start () {
+        paused = false;
+        PauseMenu = GameObject.Find("PauseMenu");
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            paused = !paused;
+        }
+
+        if (paused)
+        {
+            PauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }else if (!paused)
+        {
+            PauseMenu.SetActive(false);
+            Time.timeScale = 1;
+        
+        }
+	}
+    public void Resume()
+    {
+        paused = false;
+
+    }
+
+    public void MainMenu()
+    {
+        Application.LoadLevel(0);
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.SetInt("currentscenesave", Application.loadedLevel);
+
+    }
+
+    public void Load()
+    {
+        Application.LoadLevel(PlayerPrefs.GetInt("currentscenesave"));
+    }
+
+    public void Mute()
+    {
+        isMute = !isMute;
+        AudioListener.volume = isMute ? 0 : 1;
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void NewGame()
+    {
+        Application.LoadLevel(1);
+    }
+}
