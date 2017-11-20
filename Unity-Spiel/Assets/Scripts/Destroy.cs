e012bfcbb4b0aa6df48c219eb059f100a2c7708f
David Geigle <33023777+David-Geigle@users.noreply.github.com>
2017-11-20 17:56:26 +0100

    Marker hinzugefügt
    Marker an der Stelle, wo der Spieler sich hin bewegen soll

---
+++ Unity-Spiel/Assets/Scripts/Destroy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+	
+	void Start ()
+	{
+		Destroy(gameObject, 1.5F);
+	}
+	
+	
+	void Update ()
+	{
+		
+	}
+}
--- Unity-Spiel/Assets/Scripts/PlayerMovement.cs
+++ Unity-Spiel/Assets/Scripts/PlayerMovement.cs
@@ -1,34 +1,47 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 [RequireComponent(typeof(PlayerMotion))]
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
-    GameObject Player;
+	GameObject Player;
 	PlayerMotion motion;
+	public GameObject XMarker;
+
+	Vector3 point;
 
 	void Start()
 	{
 		motion = GetComponent<PlayerMotion>();
 	}
 
-	void Update () {
+	void Update()
+	{
 
-        if (Input.GetMouseButton(0) && !Pause.paused)
+		if (Input.GetMouseButton(0) && !Pause.paused)
 		{
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			RaycastHit hit;
 
 
-			if(Physics.Raycast(ray, out hit, 100))
+			if (Physics.Raycast(ray, out hit, 100))
 			{
 				motion.MoveToPoint(hit.point);
-
+				point = hit.point;
+			}
+			foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Marker"))
+			{
+				Destroy(obj);
 			}
 
+			Instantiate(XMarker, point, Quaternion.identity);
 		}
-        
+
+		
 
 
+	}
 
-    }
+
 }
