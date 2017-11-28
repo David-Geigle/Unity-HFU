4f37546d3757ff812a90bdc2d8f1b669b0a8ecb8
David Geigle <33023777+David-Geigle@users.noreply.github.com>
2017-11-28 13:19:15 +0100

    Menü gefixt

--- Unity-Spiel/Assets/Scripts/Blitz.cs
+++ Unity-Spiel/Assets/Scripts/Blitz.cs
@@ -1,29 +1,49 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
-public class Blitz : MonoBehaviour {
+public class Blitz : MonoBehaviour
+{
 
+	BoxCollider box;
 
-	
-	void Start ()
+	public float t = 10;
+
+	void Start()
 	{
-		
+		box = gameObject.GetComponent<BoxCollider>();
+		box.enabled = false;
 	}
-	
-	
-	void Update ()
+
+
+	void Update()
 	{
 		if (Input.GetKeyDown(KeyCode.Q) && !ZauberCooldown.isOnCooldown)
 		{
 			ZauberCooldown.isOnCooldown = true;
 			ZauberCooldown.timeRemaining = ZauberCooldown.Cooldown;
-			BlitzCast();
+			box.enabled = true;
+			StartCoroutine("BlitzCast()");
 		}
 	}
 
-	void BlitzCast()
+	IEnumerator BlitzCast()
 	{
-		
-
-
+		yield return new WaitForSeconds(t);
+		box.enabled = false;
 	}
+
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("Hit");
+		if (other.tag == "Enemy" ) // wenn das getroffene Objekt den Tag Enemy hat
+		{
+			Debug.Log("HitEnemy");
+
+		}
+	}
+
+	
 }
