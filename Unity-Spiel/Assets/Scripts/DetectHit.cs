8fc75a6ad837147a3f98a07f5b7c2367f5fc52a5
thomash23 <33208009+thomash23@users.noreply.github.com>
2017-12-19 16:12:29 +0100

    Verbesserung Schaden
    Neue Hitboxen und Schadenfunktion.

--- Unity-Spiel/Assets/Scripts/DetectHit.cs
+++ Unity-Spiel/Assets/Scripts/DetectHit.cs
@@ -5,12 +5,37 @@ using UnityEngine.UI;
 
 public class DetectHit : MonoBehaviour {
 
-	void OnTriggerEnter(Collider other)
-	{
-		GameObject.Find("Player").GetComponent<Leben>().TakeDamage(2);
-		Debug.Log("Hit");
-	}
-    
+    public GameObject Enemy;
+    private Animator anim;
+
+
+    /*    void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Enemy")
+            {
+                GameObject.Find("Player").GetComponent<Leben>().TakeDamage(2);
+                Debug.Log("Hit");
+            }
+        }*/
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.tag == "Enemy" )
+        {
+            GameObject.Find("Player").GetComponent<Leben>().TakeDamage(2);
+            Debug.Log("Hit");
+
+        }
+
+    }
+
 
 
 }
