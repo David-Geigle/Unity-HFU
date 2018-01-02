ab08ba737ce5c221c06f97089ada274404c0b592
tobifischer <33051744+tobifischer@users.noreply.github.com>
2018-01-02 18:50:22 +0100

    Portal0.2
    Portal verschoben für schnellere tests. SpawnPoint will noch nicht richtig aber die reine Teleportation klappt.

---
+++ Unity-Spiel/Assets/Scripts/EmptyCSharpFile.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmptyCSharpFile : MonoBehaviour {
+	public Transform spawnPoint;
+
+	void OnTriggerEnter(Collider other) {
+		other.gameObject.transform.position = spawnPoint.position;
+	}
+}
