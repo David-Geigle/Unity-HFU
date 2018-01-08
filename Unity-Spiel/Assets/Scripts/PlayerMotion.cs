b614f75a24fbb8740c8b95814a3c77806608bf76
David Geigle <33023777+David-Geigle@users.noreply.github.com>
2018-01-08 13:30:37 +0100

    Animationfix

--- Unity-Spiel/Assets/Scripts/PlayerMotion.cs
+++ Unity-Spiel/Assets/Scripts/PlayerMotion.cs
@@ -27,24 +27,34 @@ public class PlayerMotion : MonoBehaviour {
 		else
 			agent.isStopped = false;
 
-		if (agent.hasPath == true && Blitz.isCasting == false)
+		if (agent.hasPath == true && Blitz.isCasting == false) // laufen
 		{
 			anim.SetBool("isWalking", true);
 			anim.SetBool("isIdle", false);
 		}
 
-		else if (Blitz.isCasting == true)
+		else if (Blitz.isCasting == true)//Cast
 		{
 			anim.SetBool("isIdle", false);
-			anim.SetBool("isAttacking", true);
+			anim.SetBool("isCasting", true);
 			anim.SetBool("isWalking", false);
 			agent.ResetPath();
 		}
 
-		else
+		else if (Input.GetMouseButton(1)) //autoattack
+		{
+			anim.SetBool("isIdle", false);
+			anim.SetBool("isCasting", false);
+			anim.SetBool("isWalking", false);
+			anim.SetBool("isAttacking", true);
+			agent.ResetPath();
+		}
+		
+		else //idle
 		{
 			anim.SetBool("isWalking", false);
 			anim.SetBool("isIdle", true);
+			anim.SetBool("isCasting", false);
 			anim.SetBool("isAttacking", false);
 		}
 	}
